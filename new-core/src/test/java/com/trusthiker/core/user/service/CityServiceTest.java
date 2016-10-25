672dda3b726de58209eb647d2339baf22d0d4214
ruisheng <ruishengle@126.com>
2016-10-25 18:26:06 +0800

    单元测试

--- new-core/src/test/java/com/trusthiker/core/user/service/CityServiceTest.java
+++ new-core/src/test/java/com/trusthiker/core/user/service/CityServiceTest.java
@@ -1,6 +1,7 @@
 package com.trusthiker.core.user.service;
 
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
 import lombok.extern.slf4j.Slf4j;
 import org.dbunit.DataSourceBasedDBTestCase;
 import org.dbunit.dataset.IDataSet;
@@ -9,6 +10,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.sql.DataSource;
@@ -20,7 +22,8 @@ import static org.junit.Assert.*;
 @ContextConfiguration(locations = "classpath:spring/appContext.xml")
 @Transactional
 @TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
-        DbUnitTestExecutionListener.class })
+        DirtiesContextTestExecutionListener.class,
+        TransactionDbUnitTestExecutionListener.class })
 public class CityServiceTest {
 
 
